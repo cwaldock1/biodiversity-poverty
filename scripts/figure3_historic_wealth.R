a8c5feb576a99829ccff1bf4dc7dc7bcee356152
cwaldock1 <conorwaldock@gmail.com>
2025-02-06 18:21:33 +0100

    Update historic wealth figures

--- scripts/figure3_historic_wealth.R
+++ scripts/figure3_historic_wealth.R
@@ -1,8 +1,16 @@
-pacman::p_load(tidyverse)
+# Script to create figure 3 showing global patterns in historic wealth
 
-# load in dataset shared by Ole
+# Load packages and data ----
+
+# Load in packages
+# install first devtools::install_github("yutannihilation/ggsflabel")
+pacman::p_load(tidyverse, rnaturalearth, rnaturalearthdata, ggrepel, devtools, ggsflabel, sf, patchwork)
+
+# Load in dataset
 
 civ <- readxl::read_xlsx("data/global civilisations/civilizations and crops_final.xlsx", sheet = 1)
+
+# Process and clean data
 
 cleaned <- civ %>% 
   mutate(first_year_b2k = case_when(!is.na(first_year_BC) ~ first_year_BC+2000, 
@@ -13,19 +21,24 @@ cleaned <- civ %>%
          x = as.numeric(str_split(coordinates,',', simplify = T)[,2])) %>% 
   janitor::clean_names()
 
+# Convert to a spatial object
 
 civ_sf <- st_transform(st_as_sf(cleaned, coords = c('x', 'y'), crs = 'wgs84'), crs = 'ESRI:54009')
 
+# Load in global maps
 
-library(rnaturalearth)
-library(rnaturalearthdata)
+# Set target CRS
 
 target_crs <- st_crs("+proj=moll +lon_0=5 +x_0=0 +y_0=0 +lat_ts=45 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
+
+# Get world and transform
 
 world <- ne_countries(scale = "medium", returnclass = "sf") %>%
   filter(continent != 'Antarctica') %>% 
   st_transform(4326) %>%
   st_make_valid()
+
+# Shift the projection
 
 offset <- 180 - 5
 
@@ -39,38 +52,27 @@ polygon <- st_polygon(x = list(rbind(
   st_sfc() %>%
   st_set_crs(4326)
 
+# Fix polygons
+
 world_fixed <- world %>% st_difference(polygon)
 
+# Back transform
+
 world_fixed <- st_transform(world_fixed, target_crs)
+
+# Test plot
 
 ggplot(data = world_fixed) +
   geom_sf()
 
-
-# tm_shape(world_fixed) + 
-#  tm_borders(col = 'black') + 
-#  tm_layout(legend.outside = T, 
-#            frame = FALSE, bg.color = "transparent") + 
-#  tm_shape(civ_sf) + 
-#  tm_dots(col = 'first_year_b2k', 
-#          style = 'cont',
-#          size = 1, 
-#          palette = 'viridis')
-
-library(ggrepel)
-pacman::p_load(devtools)
-devtools::install_github("yutannihilation/ggsflabel")
-library(ggsflabel)
+# Convert civilisations to sf object
 
 civ_sf <- st_as_sf(cleaned, coords = c('x', 'y'), crs = 'wgs84')
 
-world <- ne_countries(scale = "medium", returnclass = "sf") %>%
-  filter(continent != 'Antarctica') %>% 
-  st_transform(4326) %>%
-  st_make_valid()
+# Plot with reprojection
 
-civ_world_map <- ggplot(world) + 
-  geom_sf(data = world, col = 'gray80', fill = 'gray70', inherit.aes = TRUE) + 
+civ_world_map <- ggplot(world_fixed) + 
+  geom_sf(data = world_fixed, col = 'gray80', fill = 'gray70', inherit.aes = TRUE) + 
   geom_sf(data = civ_sf %>% arrange(desc(first_year_b2k)), aes(fill = first_year_b2k, size = first_year_b2k-last_year_b2k), 
           pch = 21) + 
   geom_sf_text_repel(data = civ_sf, aes(label = name), force = 0.1, seed = 3, size = 2.5, max.overlaps=200) + 
@@ -78,14 +80,17 @@ civ_world_map <- ggplot(world) +
   theme(panel.border = element_blank()) + 
   scale_fill_viridis_c(option = 'C') + 
   theme(legend.position = 'bottom') + 
-  labs(size = 'Duration', fill = 'Years B2K') +
-  scale_y_continuous(breaks = c(-50, -25, 0, 25, 50), limits = c(-50, 70))
+  labs(size = 'Duration', fill = 'Years B2K') + 
+  xlab(NULL) + 
+  ylab(NULL)#+
+  #scale_y_continuous(breaks = c(-50, -25, 0, 25, 50), limits = c(-50, 70))
 
 png('figures/figure1/world_civilisations.png', res = 300, height = 2000, width = 3000)
 civ_world_map
 dev.off()
 
-# colour palette
+# Reference for colour palette
+
 viridis::inferno(5, begin = 0, end = 0.8)
 viridis::turbo(4)
 c(Europe, Asia, Americas, Africa)
@@ -114,20 +119,25 @@ civ_timeline <- ggplot(data = civ_sf %>%
   ylab('Latitude')
 civ_timeline
 
+# Save civilisation timeline png
+
 png(filename = 'figures/figure1/world_civilisations_timeline.png', res = 300, width = 1000, height = 1000)
 civ_timeline
 dev.off()
 
 
-### read in madison data for simpler regions and times
+# Read in madison data for simpler regions and times
+
 world_gdp <- readxl::read_xlsx("data/global civilisations/globalGDP_maddison.xlsx")
 
-# quick plot of barplots
+# Quick plot of barplots
+
 ggplot(data = world_gdp) + 
   geom_bar(aes(x = region, y = gdp), stat = 'identity') + 
   facet_wrap(~year, scales = 'free_y')
 
-# reformat for plotting on single axis
+# Reformat for plotting on single axis
+
 world_gdp <- world_gdp %>% 
   pivot_wider(names_from = region, values_from = gdp) %>% 
   pivot_longer(3:7) %>% 
@@ -137,16 +147,13 @@ world_gdp <- world_gdp %>%
                             region == 'Usa' ~ 'USA', 
                             T ~ region))
 
-# get mean latitude to join in
-world %>% 
-  filter(admin %in% c('China', 'India') | subregion %in% c('Western Europe', 'Northern Europe', 'Southern Europe', 'Northern America')) %>% 
-  pull(admin) %>% 
-  sort
+# Get mean latitude for colouration
 
 world_latitudes <- world %>% 
   filter(admin %in% c('China', 'India') | 
            subregion %in% c('Western Europe', 'Northern Europe', 'Southern Europe', 'Northern America') | 
          region_un == 'Africa') %>% 
+  # rename countries or regions to fit data together
   mutate(region_new = case_when(admin == 'China' ~ 'China', 
                                 admin == 'India' ~ 'India', 
                                 subregion %in% c('Western Europe', 'Northern Europe', 'Southern Europe') ~ 'West Europe', 
@@ -158,29 +165,19 @@ world_latitudes <- world %>%
   summarize() %>% 
   group_by(region_new) %>% 
   nest() %>% 
+  # get avearge of latitude
   mutate(coords = purrr::map(data, ~mean(st_coordinates(.)[,2], na.rm = T))) %>% 
   unnest(coords) %>% 
   select(-data)
 
 
-# join in regional latitudes
+# Join in regional latitudes
+
 world_gdp <- left_join(world_gdp, world_latitudes, by = c('region' = 'region_new'))
          
-  
-png(filename = 'figures/figure1/global_gdp_comparison.png', res = 300, width = 1000, height = 1000)
-world_gdp_plot <- ggplot(data = world_gdp) + 
-  geom_point(aes(x = year, y = perc_gdp*100, col = name), size = 5) +
-  geom_line(aes(x = year, y = perc_gdp*100, col = name), lwd = 1) + 
-  scale_colour_manual(values = viridis::inferno(5, begin = 0, end = 0.8)) + 
-  theme_bw() + 
-  ylab('Estimated % of global GDP') + 
-  xlab('Years before present') + 
-  theme(aspect.ratio = 1, 
-        panel.grid = element_blank(), 
-        legend.position = 'bottom') + 
-  labs(colour = 'region')
-world_gdp_plot
-dev.off()
+
+
+# World map of GDP comparisons through time from 1000 to 2000AD
 
 png(filename = 'figures/figure1/global_gdp_comparison_v2.png', res = 300, width = 1000, height = 1000)
 world_gdp_plot_v2 <- 
@@ -188,10 +185,6 @@ world_gdp_plot_v2 <-
   geom_vline(aes(xintercept = 1000)) + 
   geom_point(aes(x = year, y = perc_gdp*100, col = coords, group = region), size = 5) +
   geom_line(aes(x = year, y = perc_gdp*100, col = coords, group = region), lwd = 1) + 
-  #geom_text(data = world_gdp %>% filter(year == 1000), 
-  #          aes(x = year+100+c(100,0,0,80,0), y = (perc_gdp*100)+c(-0,0.2,1.5,4.5,1), col = coords, label = region, 
-  #              angle = c(-10, -10, 10, 15, 0)),
-  #          lwd = 1, hjust = 0) + 
   scale_colour_gradient2(high = "#005AB5",
                          mid = "gray80",
                          low = "#DC3220", 
@@ -210,40 +203,6 @@ civ_timeline + theme(aspect.ratio = 0.5,
 dev.off()
 
 
-world_gdp_plot_v2
-dev.off()
 
-png(filename = 'figures/figure1/global_gdp_comparison_v3.png', res = 300, width = 1000, height = 1000)
-world_gdp_plot_v3 <- ggplot(data = world_gdp %>% arrange(desc(region))) + 
-  geom_vline(aes(xintercept = 1000)) + 
-  geom_line(aes(x = year, y = coords, colour = name), lwd = 1) + 
-  geom_point(aes(x = year, y = coords, size = perc_gdp*100, colour = name)) +
-  geom_text(data = world_gdp %>% filter(year == 1000), 
-            aes(x = year+c(100,100,100,100,100), y = coords+c(3,3,3,4,3), 
-                                                           label = region), 
-            hjust = 0) + 
-  scale_colour_manual(values = viridis::inferno(5, begin = 0, end = 0.8), guide = "none") + 
-  theme_bw() + 
-  ylab('Latitue') + 
-  xlab('Years before present') + 
-  theme(aspect.ratio = 1, 
-        panel.grid = element_blank(), 
-        panel.background = element_blank(),
-        plot.background = element_blank(),
-        legend.position = 'bottom') + 
-  labs(size = '% world economy') + 
-  scale_size(range = c(0.1, 20), breaks = c(0, 2, 10, 20,40)) + 
-  scale_y_continuous(breaks = c(-50, -25, 0, 25, 50), limits = c(-50, 70))
-world_gdp_plot_v3
-dev.off()
 
-png(filename = 'figures/figure1/global_gdp_comparison_and_timeline.png', res = 300, width = 2500, height = 1500)
-civ_timeline + theme(aspect.ratio = 0.5, 
-                     legend.position = 'none') + world_gdp_plot_v2 + theme(aspect.ratio = 0.5)
-dev.off()
-
-png(filename = 'figures/figure1/global_gdp_comparison_and_timeline_v2.png', res = 300, width = 2500, height = 1500, 
-    bg = 'transparent')
-civ_timeline + theme(aspect.ratio = 0.5) + world_gdp_plot_v3
-dev.off()
 
